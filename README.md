04df8ba619546ba1e39c5c3e4a3f791920a5ee9d
JP Wesselink <jpwesselink@gmail.com>
2023-07-26 12:58:17 +0200

    updated readme, fixed lambda path

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# replace this
+# cdk-sqs-to-eventbridge
+
+This is a CDK construct library that allows you to easily put messages from an SQS queue to an EventBridge event bus
--- src/index.ts
+++ src/index.ts
@@ -20,27 +20,27 @@ export class SqsToEventbridge extends Construct {
 
     const idName = paramCase(id);
     // sqs to lambda which forwards events to the private event backbone bus
-    const yodaSqsToPrivateEventBackboneLambda = new NodejsFunction(
+    const sqsToEventBridgeLambda = new NodejsFunction(
       this,
       'sqsToEventbridgeLambda',
       {
         functionName: `${idName}-sqs-to-event-bridge-lambda`,
         runtime: Runtime.NODEJS_18_X,
         entry:
-          'src/constructs/SqsToEventbridge/lambdas/sqsToEventbridgeLambda.ts',
+          'src/lambdas/sqsToEventbridgeLambda.ts',
         handler: 'handler',
         logRetention,
       },
     );
-    yodaSqsToPrivateEventBackboneLambda.addEnvironment(
+    sqsToEventBridgeLambda.addEnvironment(
       'EVENT_BUS_NAME',
       eventBus.eventBusName,
     );
     // allow the lambda forwarder to put events on the private event backbone bus
-    eventBus.grantPutEventsTo(yodaSqsToPrivateEventBackboneLambda);
+    eventBus.grantPutEventsTo(sqsToEventBridgeLambda);
     const eventSource = new SqsEventSource(sqsQueue);
 
-    yodaSqsToPrivateEventBackboneLambda.addEventSource(eventSource);
+    sqsToEventBridgeLambda.addEventSource(eventSource);
 
     this.eventSourceMappingId = eventSource.eventSourceMappingId;
   }
