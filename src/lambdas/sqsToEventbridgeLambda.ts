b71e7673f5591e0bb10082541926a496c07fea91
JP Wesselink <jpwesselink@gmail.com>
2023-07-26 13:00:06 +0200

    fix: removed unused code

--- src/lambdas/sqsToEventbridgeLambda.ts
+++ src/lambdas/sqsToEventbridgeLambda.ts
@@ -16,7 +16,6 @@ export const handler: SQSHandler = async (event: SQSEvent) => {
     });
   }
   for (const record of event.Records) {
-    console.log('record', JSON.stringify({ record }, null, 2));
     const { body } = record;
     const sqsEvent = JSON.parse(body);
 
@@ -26,19 +25,14 @@ export const handler: SQSHandler = async (event: SQSEvent) => {
           Detail: JSON.stringify(sqsEvent.detail),
           DetailType: sqsEvent['detail-type'],
           Resources: sqsEvent.resources,
-          // ReplayName: event["replay-name"],
           Source: sqsEvent.source,
           EventBusName: env.EVENT_BUS_NAME,
-          // "arn:aws:events:eu-west-1:337129175967:event-bus/event-backbone-private-bus",
-          // EventBusName: config.eventBusName,
           /* @ts-ignore */
           ReplayName: event['replay-name'],
-          // ",
         },
       ],
     };
 
     await eventBridgeClient.send(new PutEventsCommand(params));
-    // console.log("Success, event sent; requestID:", data);
   }
 };
